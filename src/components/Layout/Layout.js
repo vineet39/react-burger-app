418b2941e4db088155e3c7046e2890f029226af9
rmit-s3734938-vineet-bugtani <s3734938@student.rmit.edu.au>
2020-09-21 20:04:55 +1000

    Implemented toolbar and navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/ToolBar/Toolbar';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, Sidedrawer</div>
-        <main>{props.children}</main>
+        <Toolbar />
+        <main className={classes.Content}>{props.children}</main>
     </Aux>
 );
 
---
+++ src/components/Navigation/ToolBar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './ToolBar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const toolbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <Logo/>
+        <nav>
+            <NavigationItems />
+        </nav>
+    </header>
+);
+
+export default toolbar;
