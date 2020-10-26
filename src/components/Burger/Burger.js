a0c573a985b91df84eff45513732453988baf4c0
rmit-s3734938-vineet-bugtani <s3734938@student.rmit.edu.au>
2020-10-26 22:00:16 +1100

    Refactoring code

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import icons from '../Order/Order';
+
 
 const burger = (props) => {
-    let ingredients = Object.keys(props.ingredients).map(name => {
-        return [...Array(props.ingredients[name])].map((_, i) => {
-            return <BurgerIngredient key={name + i} type={name} />
-        });
-    }).reduce((total, amount) => {
-        return total.concat(amount);
-    });
-    if(ingredients.length ===0) {
-        ingredients = <p>Please add some items</p>
+    let items = [];
+    const ingredients = { ...props.ingredients, 'test':'test'};
+    for (var name in ingredients) {
+        if (ingredients.hasOwnProperty(name)) {
+            let value = ingredients[name];
+            if (value > 0) {
+                for (let i = 0; i <= value - 1; i++) {
+                    items.push(<BurgerIngredient key={name + i} type={name} />);
+                }
+            }
+        }
     }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            {ingredients}
+            {items.length === 0 ? <p>Please add some items</p> : items}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
