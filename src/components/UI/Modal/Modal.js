5926b477bea5bf37777fc9d784baaf719e5c11ee
rmit-s3734938-vineet-bugtani <s3734938@student.rmit.edu.au>
2020-10-26 21:59:33 +1100

    Removing commented code

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,21 +3,6 @@ import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
-// const modal = (props) => (
-//     <Aux>
-//         <Backdrop show={props.show} clicked={props.modalClosed} />
-//         <div
-//             className={classes.Modal}
-//             style={{
-//                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-//                 opacity: props.show ? '1' : '0'
-//             }}>
-//             <button className={classes.CloseButtonIcon} onClick={props.modalClosed}><i class="fa fa-times-circle"></i></button>
-//             {props.children}
-//         </div>
-//     </Aux>
-// );
-
 class Modal extends Component {
     shouldComponentUpdate(nextProps) {
         return nextProps.show !== this.props.show
